d1e8ee7703f60ce910bf2e07e120cccd203d7688
GPBatch <46971223+GPBatch@users.noreply.github.com>
2019-01-28 16:59:49 -0800

    Delete updateFit.m

--- am_put_oracle/updateFit.m
+++
@@ -1,5 +0,0 @@
-function fit = updateFit(fit, add_grid, add_mean, add_var, r_cur)
-fit.x = [fit.x; add_grid];
-fit.y = [fit.y; add_mean];
-fit.r = [fit.r; r_cur];
-% fit.gp.lik.sigma2 = [fit.gp.lik.sigma2; add_var];
